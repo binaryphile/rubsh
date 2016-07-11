024384799ccac0eb65e061fa2b5dda399faf5a4e
Ted Lilley <ted.lilley@accelerated.com>
2016-07-11 14:39:35 -0400

    refactor: simpler source directory detection

--- lib/core.sh
+++ lib/core.sh
@@ -6,6 +6,7 @@
 # shellcheck disable=SC2046,SC2155
 declare -r _rubsh_core="$(set -- $(sha1sum "$BASH_SOURCE"); printf "%s" "$1")"
 
+# TODO: move these three to the shell lib
 _rubsh.core.abspath() { (cd "$1" >/dev/null 2>&1; printf "%s" "$PWD")                         ;}
 _rubsh.core.lib_dir() { printf "%s" "$(_rubsh.core.abspath "$(_rubsh.core.source_location)")" ;}
 
@@ -14,14 +15,14 @@ _rubsh.core.source_location() {
 
   [[ -d $dir ]] || {
     printf "%s" "$PWD"
-    return 0
+    return
   }
   printf "%s" "$dir"
 }
 
 # https://stackoverflow.com/questions/192292/bash-how-best-to-include-other-scripts/12694189#12694189
 # shellcheck disable=SC2155
-[[ -n $_rubsh_lib ]]  || declare -r _rubsh_lib="$(_rubsh.core.lib_dir)"
+[[ -n $_rubsh_lib ]]  || declare -r _rubsh_lib="$(cd "${BASH_SOURCE%/*}" >/dev/null 2>&1; printf "%s" "$PWD")" # I think this doesn't work correctly if the declare -r comes after the assignment...weird scoping
 
 # https://stackoverflow.com/questions/10582763/how-to-return-an-array-in-bash-without-using-globals/15982208#15982208
 # Print array definition to use with assignments, for loops, etc.
