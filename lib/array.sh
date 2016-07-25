6a1b96f3aaf740ef795fff4a0b9c0f1a2dd0f340
Ted Lilley <ted.lilley@accelerated.com>
2016-07-25 16:14:44 -0400

    fix: working array tests

--- lib/array.sh
+++ lib/array.sh
@@ -13,125 +13,124 @@ _rubsh_init() {
 
   # shellcheck disable=SC2034
   read -d "" -a aliases <<EOS
-to_s
+:to_s
 EOS
 
-  _rubsh.core.alias Array aliases
+  _rubsh.core.alias :Array :aliases
 }
 
 _rubsh_init
 unset -f _rubsh_init
 
-_rubsh.Shell.alias_function Array.== Array.eql?
+_rubsh.Shell.alias_function :Array.== :Array.eql?
 
 Array.eql? () {
-  local _rubsh_ary1="$1"
-  local _rubsh_ary2="$2"
-  local _rubsh_i
+  local _rubsh_disney=$1
+  local _rubsh_august=$2
+  local _rubsh_coconut
 
-  _rubsh.Shell.dereference "_rubsh_ary1"
-  _rubsh.Shell.dereference "_rubsh_ary2"
+  _rubsh.Shell.dereference :_rubsh_disney
+  _rubsh.Shell.dereference :_rubsh_august
 
-  (( ${#_rubsh_ary1[@]} == "${#_rubsh_ary2[@]}" )) || return
-  for _rubsh_i in "${!_rubsh_ary1[@]}"; do
-    [[ ${_rubsh_ary1[$_rubsh_i]} == "${_rubsh_ary2[$_rubsh_i]}" ]] || return
+  (( ${#_rubsh_disney[@]} == "${#_rubsh_august[@]}" )) || return
+  for _rubsh_coconut in "${!_rubsh_disney[@]}"; do
+    [[ ${_rubsh_disney[$_rubsh_coconut]} == "${_rubsh_august[$_rubsh_coconut]}" ]] || return
   done
 }
 
 # https://stackoverflow.com/questions/3685970/check-if-an-array-contains-a-value#answer-8574392
 Array.include? () {
-  local _rubsh_ary="$1"
-  local _rubsh_item
+  local _rubsh_belgium=$1
+  local _rubsh_bruno
 
-  _rubsh.Shell.dereference "_rubsh_ary"
+  _rubsh.Shell.dereference :_rubsh_belgium
 
-  for _rubsh_item in "${_rubsh_ary[@]}"; do
-    [[ $_rubsh_item != "$2" ]] || return
+  for _rubsh_bruno in "${_rubsh_belgium[@]}"; do
+    [[ $_rubsh_bruno != "$2" ]] || return 0
   done
   return 1
 }
 
 Array.index() {
-  local -a _rubsh_ary
-  local _rubsh_i
-  local _rubsh_item="$2"
+  local _rubsh_arizona=$1
+  local _rubsh_delta
 
-  _rubsh_ary=( $( _rubsh.Shell.value "$1") )
-  for _rubsh_i in "${!_rubsh_ary[@]}"; do
-    [[ ${_rubsh_ary[${_rubsh_i}]} != "$_rubsh_item" ]] || { _rubsh.IO.puts _rubsh_i; return ;}
+  _rubsh.Shell.dereference :_rubsh_arizona
+  for _rubsh_delta in "${!_rubsh_arizona[@]}"; do
+    [[ ${_rubsh_arizona[$_rubsh_delta]} != "$2" ]] || { _rubsh.IO.puts "$_rubsh_delta"; return ;}
   done
   return 1
 }
 
 # https://stackoverflow.com/questions/1527049/bash-join-elements-of-an-array#answer-17841619
 Array.join() {
-  local delim="$2"
+  local _rubsh_carol=$1
+  local _rubsh_balance=$2
 
-  ! _rubsh.Shell.variable? "$delim" || _rubsh.Shell.dereference delim
+  _rubsh.Shell.dereference :_rubsh_carol
+  ! _rubsh.Shell.variable? "$_rubsh_balance" || _rubsh.Shell.dereference :_rubsh_balance
   # shellcheck disable=SC2046
-  set -- $(_rubsh.Shell.value "$1")
+  set -- "${_rubsh_carol[@]}"
   printf "%s" "$1"
   shift
-  printf "%s" "${@/#/$delim}"
+  printf "%s" "${@/#/$_rubsh_balance}"
 }
 
 Array.new() {
-  local _rubsh_arg
-  local _rubsh_method
-  local _rubsh_methods
-  local _rubsh_val
+  local _rubsh_brown
+  local _rubsh_audio
+  local _rubsh_ballad
+  local _rubsh_charter
 
   # shellcheck disable=SC2034
-  read -d "" -a _rubsh_methods <<EOS
-==
-eql?
-include?
-index
-join
-remove
-slice
-to_s
+  read -d "" -a _rubsh_ballad <<EOS
+:==
+:delete
+:eql?
+:include?
+:index
+:join
+:slice
+:to_s
 EOS
 
-  for _rubsh_method in "${_rubsh_methods[@]}"; do
-    _rubsh.core.alias_method "$1" "$_rubsh_method" "Array"
+  for _rubsh_audio in "${_rubsh_ballad[@]}"; do
+    _rubsh.core.alias_method "$1" "$_rubsh_audio" :Array
   done
 
   (( $# > 1 )) || return 0
-  local "$1" && {
-    _rubsh_arg="$1"
+
+  local "$(_rubsh.Symbol.to_s "$1")" && {
+    _rubsh_brown=$1
     shift
     (( $# > 1 )) || {
-      eval "_rubsh_val=$1"
-      _rubsh.Shell.passback_as "$_rubsh_arg" "${_rubsh_val[@]}"
+      _rubsh.Shell.assign_literal :_rubsh_charter "$1"
+      _rubsh.Shell.passback_as "$_rubsh_brown" "${_rubsh_charter[@]}"
       return 0
     }
-    _rubsh.Shell.passback_as "$_rubsh_arg" "$@"
+    _rubsh.Shell.passback_as "$_rubsh_brown" "$@"
   }
 }
 
-Array.remove() {
-  local i
-  local item
-  local result
+Array.delete() {
+  local _rubsh_arnold=$1
+  local _rubsh_corona
+  local _rubsh_clock
+  local _rubsh_ecology=$2
 
-  item="$2"
-  # shellcheck disable=SC2046
-  set -- $(_rubsh.Shell.value "$1")
-  result=( )
-  for i in "$@"; do
-    [[ $i == "$item" ]] || result+=( "$i" )
+  _rubsh.Shell.dereference :_rubsh_arnold
+  _rubsh_clock=( )
+  for _rubsh_corona in "${_rubsh_arnold[@]}"; do
+    [[ $_rubsh_corona == "$_rubsh_ecology" ]] || _rubsh_clock+=( "$_rubsh_corona" )
   done
-  # TODO: use _Array.to_s?
-  cat <<< "${result[@]}"
+  local "$(_rubsh.Symbol.to_s "$1")" && _rubsh.Shell.passback_as "$1" "${_rubsh_clock[@]}"
 }
 
 Array.slice() {
-  local first=$2
-  local last=$3
-  local array
+  local _rubsh_acrobat=$3
+  local _rubsh_austria=$4
+  local _rubsh_elegant=$1
 
-  eval "local array=(\"\${$1[@]}\")"
-
-  cat <<< "${array[@]:${first}:$((last-first+1))}"
+  _rubsh.Shell.dereference :_rubsh_elegant
+  local "$(_rubsh.Symbol.to_s "$2")" && _rubsh.Shell.passback_as "$2" "${_rubsh_elegant[@]:${_rubsh_acrobat}:$((_rubsh_austria-_rubsh_acrobat+1))}"
 }
--- shpec/array_shpec.sh
+++ shpec/array_shpec.sh
@@ -9,7 +9,7 @@ describe "Array.=="
     (
     sample=( "a" "b" "c" )
     expected=( "a" "b" "c" )
-    Array.== sample expected
+    Array.== :sample :expected
     assert equal $? 0
     )
   end
@@ -18,7 +18,7 @@ describe "Array.=="
     (
     sample=( "a" "b" "c" )
     expected=( "a" "b" "d" )
-    Array.== sample expected
+    Array.== :sample :expected
     assert equal $? 1
     )
   end
@@ -27,8 +27,30 @@ describe "Array.=="
     (
     sample=( "a" "b" "c" )
     expected=( "a" "b" "c" "d" )
-    Array.== sample expected
+    Array.== :sample :expected
     assert equal $? 1
+    )
+  end
+end
+
+describe "Array.delete"
+  it "deletes the last element of an array on exact match"
+    (
+    # shellcheck disable=SC2034
+    sample=( "a" "b" "c" )
+    Array.delete :sample "c"
+    assert equal "$(_rubsh.Shell.class :sample)" "array"
+    assert equal "${sample[*]}" "a b"
+    )
+  end
+
+  it "doesn't delete elements which aren't there"
+    (
+    # shellcheck disable=SC2034
+    sample=( "a" "b" "c" )
+    Array.delete :sample "d"
+    assert equal "$(_rubsh.Shell.class :sample)" "array"
+    assert equal "${sample[*]}" "a b c"
     )
   end
 end
@@ -38,7 +60,7 @@ describe "Array.eql?"
     (
     sample=( "a" "b" "c" )
     expected=( "a" "b" "c" )
-    Array.eql? sample expected
+    Array.eql? :sample :expected
     assert equal $? 0
     )
   end
@@ -47,7 +69,7 @@ describe "Array.eql?"
     (
     sample=( "a" "b" "c" )
     expected=( "a" "b" "d" )
-    Array.eql? sample expected
+    Array.eql? :sample :expected
     assert equal $? 1
     )
   end
@@ -55,8 +77,9 @@ describe "Array.eql?"
   it "detects inequality of two unequal-length arrays"
     (
     sample=( "a" "b" "c" )
+    # shellcheck disable=SC2034
     expected=( "a" "b" "c" "d" )
-    Array.eql? sample expected
+    Array.eql? :sample :expected
     assert equal $? 1
     )
   end
@@ -66,9 +89,9 @@ describe "Array.include?"
   it "tests for element membership"
     (
     # shellcheck disable=SC2034
-    sample=( a b c )
-    Array.include? sample "a"
-    assert equal "$?" 0
+    sample=( "a" "b" "c" )
+    Array.include? :sample "a"
+    assert equal $? 0
     )
   end
 end
@@ -78,7 +101,7 @@ describe "Array.index"
     (
     # shellcheck disable=SC2034
     sample=( a b c )
-    assert equal "$(Array.index sample "a")" 0
+    assert equal "$(Array.index :sample "a")" 0
     )
   end
 
@@ -86,7 +109,7 @@ describe "Array.index"
     (
     # shellcheck disable=SC2034
     sample=( a b c )
-    assert equal "$(Array.index sample "b")" 1
+    assert equal "$(Array.index :sample "b")" 1
     )
   end
 end
@@ -96,7 +119,7 @@ describe "Array.join"
     (
     # shellcheck disable=SC2034
     sample=( "a" "b" "c" )
-    assert equal "$(Array.join sample "|")" "a|b|c"
+    assert equal "$(Array.join :sample "|")" "a|b|c"
     )
   end
 
@@ -106,7 +129,7 @@ describe "Array.join"
     sample=( "a" "b" "c" )
     # shellcheck disable=SC2034
     delimiter="|"
-    assert equal "$(Array.join sample delimiter)" "a|b|c"
+    assert equal "$(Array.join :sample :delimiter)" "a|b|c"
     )
   end
 
@@ -114,7 +137,7 @@ describe "Array.join"
     (
     # shellcheck disable=SC2034
     sample=( "a" "b" "c" )
-    assert equal "$(Array.join sample " | ")" "a | b | c"
+    assert equal "$(Array.join :sample " | ")" "a | b | c"
     )
   end
 end
@@ -124,7 +147,7 @@ describe "Array.new"
     (
     # shellcheck disable=SC2034
     sample=( "" )
-    Array.new sample
+    Array.new :sample
     sample.include? "a"
     assert equal $? 1
     )
@@ -134,21 +157,29 @@ describe "Array.new"
     (
     # shellcheck disable=SC2034
     declare sample
-    Array.new sample "a" "b" "c"
-    expected=( "a" "b" "c" )
-    sample.eql? expected
-    assert equal $? 0
+    Array.new :sample "a" "b" "c"
+    assert equal "$(_rubsh.Shell.class :sample)" "array"
+    assert equal "${sample[*]}" "a b c"
     )
   end
 
-  it "allows a string initializer"
+  it "allows a string initializer with a literal"
     (
     # shellcheck disable=SC2034
-    declare sample
-    Array.new sample '( "a" "b" "c" )'
-    expected=( "a" "b" "c" )
-    sample.eql? expected
-    assert equal $? 0
+    declare -a sample
+    Array.new :sample '( "a" "b" "c" )'
+    assert equal "$(_rubsh.Shell.class :sample)" "array"
+    assert equal "${sample[*]}" "a b c"
+    )
+  end
+
+  it "allows a string initializer with a single value"
+    (
+    # shellcheck disable=SC2034
+    declare -a sample
+    Array.new :sample "a"
+    assert equal "$(_rubsh.Shell.class :sample)" "array"
+    assert equal "${sample[*]}" "a"
     )
   end
 
@@ -156,7 +187,7 @@ describe "Array.new"
     (
     # shellcheck disable=SC2034
     sample=( "a" "b" )
-    Array.new sample
+    Array.new :sample
     sample.include? "b"
     assert equal $? 0
     )
@@ -166,7 +197,7 @@ describe "Array.new"
     (
     # shellcheck disable=SC2034
     sample=( "a" "b" )
-    Array.new sample
+    Array.new :sample
     assert equal "$(sample.index "b")" "1"
     )
   end
@@ -175,33 +206,12 @@ describe "Array.new"
     (
     # shellcheck disable=SC2034
     sample=( "a" "b" )
-    Array.new sample
+    Array.new :sample
     # shellcheck disable=SC2034
+    result=""
     result="$(sample.join "a")"
-    result=( $(sample.remove "a") )
-    result=( $(sample.slice 1 1 ) )
-    )
-  end
-end
-
-describe "Array.remove"
-  it "removes the last element of an array on exact match"
-    (
-    # shellcheck disable=SC2034
-    sample=( "a" "b" "c" )
-    result=( $(Array.remove sample "c") )
-    expected=( "a" "b" )
-    assert equal "${result[*]}" "${expected[*]}"
-    )
-  end
-
-  it "doesn't remove elements which aren't there"
-    (
-    # shellcheck disable=SC2034
-    sample=( "a" "b" "c" )
-    result=( $(Array.remove sample "d") )
-    expected=( "a" "b" "c" )
-    assert equal "${result[*]}" "${expected[*]}"
+    sample.delete "a"
+    sample.slice :result 1 1
     )
   end
 end
@@ -211,9 +221,9 @@ describe "Array.slice"
     (
     # shellcheck disable=SC2034
     sample=( "a" "b" "c" )
-    result=( $(Array.slice sample 1 1) )
-    expected=( "b" )
-    assert equal "${result[*]}" "${expected[*]}"
+    result=""
+    Array.slice :sample :result 1 1
+    assert equal "${result[*]}" "b"
     )
   end
 end
@@ -224,7 +234,7 @@ describe "Array.to_s"
     # shellcheck disable=SC2034
     sample=( "a" "b" "c" )
     # shellcheck disable=SC2034
-    result="$(Array.to_s sample)"
+    result="$(Array.to_s :sample)"
     assert equal $? 0
     )
   end
