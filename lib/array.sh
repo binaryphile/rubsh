3dbc82ded23d85330d600cabe298f442d0e8c460
Ted Lilley <ted.lilley@accelerated.com>
2016-08-11 09:44:03 -0400

    refactor: variable names

--- lib/array.sh
+++ lib/array.sh
@@ -4,7 +4,7 @@
 [[ -z $_rubsh_array ]] || return 0
 
 # shellcheck disable=SC2046
-readonly _rubsh_array="$(set -- $(sha1sum "$BASH_SOURCE"); echo "$1")"
+readonly _rubsh_array=$(set -- $(sha1sum "$BASH_SOURCE"); echo "$1")
 
 source "${BASH_SOURCE%/*}"/core.sh 2>/dev/null || source core.sh
 
@@ -25,65 +25,67 @@ unset -f _rubsh_init
 _rubsh.Shell.alias_function :Array.== :Array.eql?
 
 Array.eql? () {
-  local _rubsh_disney=$1
-  local _rubsh_august=$2
-  local _rubsh_coconut
+  local _rubsh_aeq_array1=$1
+  local _rubsh_aeq_array2=$2
+  local _rubsh_aeq_i
 
-  _rubsh.Shell.dereference :_rubsh_disney
-  _rubsh.Shell.dereference :_rubsh_august
+  _rubsh.Shell.dereference :_rubsh_aeq_array1
+  _rubsh.Shell.dereference :_rubsh_aeq_array2
 
-  (( ${#_rubsh_disney[@]} == "${#_rubsh_august[@]}" )) || return
-  for _rubsh_coconut in "${!_rubsh_disney[@]}"; do
-    [[ ${_rubsh_disney[$_rubsh_coconut]} == "${_rubsh_august[$_rubsh_coconut]}" ]] || return
+  (( ${#_rubsh_aeq_array1[@]} == "${#_rubsh_aeq_array2[@]}" )) || return
+  for _rubsh_aeq_i in "${!_rubsh_aeq_array1[@]}"; do
+    [[ ${_rubsh_aeq_array1[$_rubsh_aeq_i]} == "${_rubsh_aeq_array2[$_rubsh_aeq_i]}" ]] || return
   done
 }
 
 # https://stackoverflow.com/questions/3685970/check-if-an-array-contains-a-value#answer-8574392
 Array.include? () {
-  local _rubsh_belgium=$1
-  local _rubsh_bruno
+  local _rubsh_ain_array=$1
+  local _rubsh_ain_item
 
-  _rubsh.Shell.dereference :_rubsh_belgium
+  _rubsh.Shell.dereference :_rubsh_ain_array
 
-  for _rubsh_bruno in "${_rubsh_belgium[@]}"; do
-    [[ $_rubsh_bruno != "$2" ]] || return 0
+  for _rubsh_ain_item in "${_rubsh_ain_array[@]}"; do
+    [[ $_rubsh_ain_item != "$2" ]] || return 0
   done
   return 1
 }
 
 Array.index() {
-  local _rubsh_arizona=$1
-  local _rubsh_delta
+  local _rubsh_aix_array=$1
+  local _rubsh_aix_i
 
-  _rubsh.Shell.dereference :_rubsh_arizona
-  for _rubsh_delta in "${!_rubsh_arizona[@]}"; do
-    [[ ${_rubsh_arizona[$_rubsh_delta]} != "$2" ]] || { _rubsh.IO.puts "$_rubsh_delta"; return ;}
+  _rubsh.Shell.dereference :_rubsh_aix_array
+  for _rubsh_aix_i in "${!_rubsh_aix_array[@]}"; do
+    [[ ${_rubsh_aix_array[$_rubsh_aix_i]} != "$2" ]] || {
+      _rubsh.IO.puts "$_rubsh_aix_i"
+      return
+    }
   done
   return 1
 }
 
 # https://stackoverflow.com/questions/1527049/bash-join-elements-of-an-array#answer-17841619
 Array.join() {
-  local _rubsh_carol=$1
-  local _rubsh_balance=$2
+  local _rubsh_ajo_array=$1
+  local _rubsh_ajo_delimiter=$2
 
-  _rubsh.Shell.dereference :_rubsh_carol
-  ! _rubsh.Shell.variable? "$_rubsh_balance" || _rubsh.Shell.dereference :_rubsh_balance
-  # shellcheck disable=SC2046
-  set -- "${_rubsh_carol[@]}"
+  _rubsh.Shell.dereference :_rubsh_ajo_array
+  ! _rubsh.Shell.variable? "$_rubsh_ajo_delimiter" || _rubsh.Shell.dereference :_rubsh_ajo_delimiter
+  set -- "${_rubsh_ajo_array[@]}"
   printf "%s" "$1"
   shift
-  printf "%s" "${@/#/$_rubsh_balance}"
+  printf "%s" "${@/#/$_rubsh_ajo_delimiter}"
 }
 
 Array.new() {
-  local _rubsh_brown
-  local _rubsh_audio
-  local _rubsh_ballad
-  local _rubsh_charter
+  local _rubsh_ane_initializer
+  local _rubsh_ane_method
+  local _rubsh_ane_methods
+  local _rubsh_ane_return_ref
 
   # shellcheck disable=SC2034
-  read -d "" -a _rubsh_ballad <<EOS
+  read -d "" -a _rubsh_ane_methods <<EOS
 :==
 :delete
 :eql?
@@ -94,43 +96,42 @@ Array.new() {
 :to_s
 EOS
 
-  for _rubsh_audio in "${_rubsh_ballad[@]}"; do
-    _rubsh.core.alias_method "$1" "$_rubsh_audio" :Array
+  for _rubsh_ane_method in "${_rubsh_ane_methods[@]}"; do
+    _rubsh.core.alias_method "$1" "$_rubsh_ane_method" :Array
   done
 
   (( $# > 1 )) || return 0
 
-  local "$(_rubsh.Symbol.to_s "$1")" && {
-    _rubsh_brown=$1
-    shift
-    (( $# > 1 )) || {
-      _rubsh.Shell.assign_literal :_rubsh_charter "$1"
-      _rubsh.Shell.passback_as "$_rubsh_brown" "${_rubsh_charter[@]}"
-      return 0
-    }
-    _rubsh.Shell.passback_as "$_rubsh_brown" "$@"
+  local "$(_rubsh.Symbol.to_s "$1")"
+  _rubsh_ane_return_ref=$1
+  shift
+  (( $# > 1 )) || {
+    _rubsh.Shell.assign_literal :_rubsh_ane_initializer "$1"
+    _rubsh.Shell.passback_as "$_rubsh_ane_return_ref" "${_rubsh_ane_initializer[@]}"
+    return 0
   }
+  _rubsh.Shell.passback_as "$_rubsh_ane_return_ref" "$@"
 }
 
 Array.delete() {
-  local _rubsh_arnold=$1
-  local _rubsh_corona
-  local _rubsh_clock
-  local _rubsh_ecology=$2
+  local _rubsh_ade_array=$1
+  local _rubsh_ade_item
+  local _rubsh_ade_result
+  local _rubsh_ade_target=$2
 
-  _rubsh.Shell.dereference :_rubsh_arnold
-  _rubsh_clock=( )
-  for _rubsh_corona in "${_rubsh_arnold[@]}"; do
-    [[ $_rubsh_corona == "$_rubsh_ecology" ]] || _rubsh_clock+=( "$_rubsh_corona" )
+  _rubsh.Shell.dereference :_rubsh_ade_array
+  _rubsh_ade_result=( )
+  for _rubsh_ade_item in "${_rubsh_ade_array[@]}"; do
+    [[ $_rubsh_ade_item == "$_rubsh_ade_target" ]] || _rubsh_ade_result+=( "$_rubsh_ade_item" )
   done
-  local "$(_rubsh.Symbol.to_s "$1")" && _rubsh.Shell.passback_as "$1" "${_rubsh_clock[@]}"
+  local "$(_rubsh.Symbol.to_s "$1")" && _rubsh.Shell.passback_as "$1" "${_rubsh_ade_result[@]}"
 }
 
 Array.slice() {
-  local _rubsh_acrobat=$3
-  local _rubsh_austria=$4
-  local _rubsh_elegant=$1
+  local _rubsh_asl_start=$3
+  local _rubsh_asl_length=$4
+  local _rubsh_asl_array=$1
 
-  _rubsh.Shell.dereference :_rubsh_elegant
-  local "$(_rubsh.Symbol.to_s "$2")" && _rubsh.Shell.passback_as "$2" "${_rubsh_elegant[@]:${_rubsh_acrobat}:$((_rubsh_austria-_rubsh_acrobat+1))}"
+  _rubsh.Shell.dereference :_rubsh_asl_array
+  local "$(_rubsh.Symbol.to_s "$2")" && _rubsh.Shell.passback_as "$2" "${_rubsh_asl_array[@]:${_rubsh_asl_start}:$((_rubsh_asl_length-_rubsh_asl_start+1))}"
 }
--- lib/keyword.sh
+++ lib/keyword.sh
@@ -4,7 +4,7 @@
 [[ -z $_rubsh_keyword ]] || return 0
 
 # shellcheck disable=SC2046
-readonly _rubsh_keyword="$(set -- $(sha1sum "$BASH_SOURCE"); echo "$1")"
+readonly _rubsh_keyword=$(set -- $(sha1sum "$BASH_SOURCE"); echo "$1")
 
 source "${BASH_SOURCE%/*}"/core.sh 2>/dev/null || source core.sh
 
@@ -17,21 +17,24 @@ __FILE__() {
 }
 
 new() {
-  local _rubsh_basic=$1
-  local _rubsh_chief=$3
-  local _rubsh_couple=$4
+  local ref=$1
+  local constructor=$3
+  local initializer=$4
 
   shift 3
-  _rubsh.String.new :_rubsh_couple
-  _rubsh_couple.chomp
-  _rubsh_couple.start_with? "(" && _rubsh_couple.end_with? ")" && shift && set -- "\"$_rubsh_couple\"" "$@"
-  echo eval "$_rubsh_chief" :"$_rubsh_basic" "$@"
+  _rubsh.String.new :initializer
+  initializer.chomp
+  ! initializer.start_with? "(" || ! initializer.end_with? ")" || {
+    shift
+    set -- "\"$initializer\"" "$@"
+  }
+  eval "$constructor" :"$ref" "$@"
 }
 
 require() {
-  local _rubsh_cuba="$PATH"
+  local path=$PATH
 
-  PATH="$RUBSH_PATH${RUBSH_PATH:+:}$PATH"
+  PATH=$RUBSH_PATH${RUBSH_PATH:+:}$PATH
   source "$1".sh 2>/dev/null
-  PATH="$_rubsh_cuba"
+  PATH="$path"
 }
