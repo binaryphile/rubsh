e72d90197797617147374e10b1ea13d85977cb14
Ted Lilley <ted.lilley@accelerated.com>
2017-03-23 21:33:49 -0400

    start over

--- lib/constant.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Functions to emulate Ruby constants
-
-[[ -z $_rubsh_constant ]] || return 0
-
-# shellcheck disable=SC2046
-readonly _rubsh_constant="$(set -- $(sha1sum "$BASH_SOURCE"); echo "$1")"
-
-source "${BASH_SOURCE%/*}"/core.sh 2>/dev/null || source core.sh
