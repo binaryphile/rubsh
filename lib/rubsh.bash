d873f09cc6d59113178f18ed5ca95df3be0dc79d
Ted Lilley <ted.lilley@accelerated.com>
2017-03-25 17:54:32 -0400

    rework

--- lib/rubsh.bash
+++ lib/rubsh.bash
@@ -1,161 +1,186 @@
 [[ -n ${_rubsh:-} ]] && return
 readonly _rubsh=loaded
 
-unset -v __ __methodh __parenth
-declare -Ag __methodh __parenth
+unset -v __ __bodyh __methodh __parenth
+declare -Ag __bodyh __methodh __parenth
 __=''
 
-class () {
-  __class=$1 # global
-  local __parent=${3:-}
+__methodh[Class]=new
+IFS=$'\n' read -rd '' __bodyh[Class.new] <<'end' ||:
+  local self=$1
+  local statement
 
-  __inherit Class "$__class"
-  [[ -n $__parent ]] && { __parenth[$__class]=$__parent; return ;}
-  if [[ $__class != 'Object' ]]; then __parenth[$__class]=Object; fi
+  printf -v statement '%s () { :;}' "$self"
+  eval "$statement"
+end
+
+Class () {
+  local method=$1; shift
+  local statement
+
+  [[ ${__methodh[Class]} == *$method* ]] || return
+  printf -v statement '__ () { %s ;}; __ "$@"' "${__bodyh[Class.$method]}"
+  eval "$statement"
 }
 
-def () {
-  local method=$1
-  local body=${2:-$(</dev/stdin)}
-  local tmp=()
+# __dispatch () {
+#   local class=$1
+#   local self=$2
+#   local method=$3
+#
+#   while [[ ${__methodh[$class]} != *$method* && -n ${__parenth[$class]} ]]; do
+#     class=${__parenth[$class]}
+#   done
+#   [[ -n ${__methodh[$class]} ]] || return
+#   "$class" "${__methodh[$class]}" "$self"
+# }
+#
+# filename each
+# { File filename $method ;}
+# File 
+# { self=$1; method=$2; shift 2; eval ${bodyh[$self.$method]} ;}
 
-  eval "$__class.$method () { $body ;}"
-  tmp=( ${__methodh[$__class]} "$method" )
-  __methodh[$__class]=${tmp[*]}
-}
-
-__inherit () {
-  local class=$1
-  local self=$2
-  local method
-
-  for method in ${__methodh[$class]}; do
-    eval "$self.$method () { $class.$method $self "'"$@"'" ;}"
-  done
-}
-
-class Class; {
-  def new <<'  end'
-    local class=$1; shift
-    local self
-
-    for self in "$@"; do
-      [[ -n ${__parenth[$class]} ]] && eval "${__parenth[$class]}".new "$@"
-      __inherit "$class" "$self"
-    done
-  end
-}
-
-class Object , Class; {
-  def set <<'  end'
-    local -n __self=$1; shift
-
-    __=''
-    "$@"
-    eval __self="$__"
-  end
-
-  def to_s <<'  end'
-    local self=$1
-
-    __=$(declare -p "$self" 2>/dev/null) || return
-    __=${__#declare -* $self=}
-  end
-}
-
-class Array; {
-  def append <<'  end'
-    local -n __vals=$1; shift
-    local __statement
-
-    case $# in
-      '0' ) return;;
-      '1' )
-        printf -v __statement '__vals+=( "${%s[@]}" )' "$1"
-        eval "$__statement"
-        ;;
-      * ) "$@"; __vals+=( "${__[@]}" );;
-    esac
-  end
-
-  def join <<'  end'
-    local -n __vals=$1; shift
-    local IFS=$1
-
-    __=${__vals[*]}
-  end
-}
-
-class Hash; {
-  def map <<'  end'
-    local -n __valh=$1
-    local __keyparm=$3
-    local __valparm=$4
-    local __lambda=$5
-    local "$__keyparm"
-    local "$__valparm"
-    local __key
-    local __retvals=()
-    local __statement
-
-    printf -v __statement '__retvals+=( "$(puts "%s")" )' "$__lambda"
-
-    for __key in "${!__valh[@]}"; do
-      printf -v "$__keyparm" '%s' "$__key"
-      printf -v "$__valparm" '%s' "${__valh[$__key]}"
-      eval "$__statement"
-    done
-
-    __=( "${__retvals[@]}" )
-  end
-}
-
-class String
-
-defs () { IFS=$'\n' read -rd '' "$1" ||: ;}
-
-puts () { printf '%s\n' "$1" ;}
-
-class File , Path; {
-  def each <<'  end'
-    local __filename=${!1}
-    local __lineparm=$3
-    local __lambda=${4:-$(</dev/stdin)}
-    local "$__lineparm"
-
-    while read -r "$__lineparm"; do
-      eval "$__lambda"
-    done <"$__filename" ||:
-  end
-
-  def write <<'  end'
-    local __filename=${!1}; shift
-    local __string
-
-    case $# in
-      '0' ) return              ;;
-      '1' ) __string=${!1}      ;;
-      *   ) "$@"; __string=$__  ;;
-    esac
-    puts "$__string" >"$__filename"
-  end
-}
-
-class Path; {
-  def expand_path <<'  end'
-    local __pathname=${!1}
-    local __filename
-
-    unset -v CDPATH
-    [[ -e $__pathname ]] && {
-      __filename=$(basename "$__pathname")
-      __pathname=$(dirname "$__pathname")
-    }
-    [[ -d $__pathname ]] || return
-    __pathname=$(cd "$__pathname"; pwd)
-    __=$__pathname${__filename:+/}${__filename:-}
-  end
-}
+# class () {
+#   __class=$1 # global
+#   local __parent=${3:-}
+#
+#   [[ -n $__parent ]] && { __parenth[$__class]=$__parent; return ;}
+#   if [[ $__class != 'Object' ]]; then __parenth[$__class]=Object; fi
+# }
+#
+# def () {
+#   local method=$1
+#   local body=${2:-$(</dev/stdin)}
+#   local tmp=()
+#
+#   eval "$__class.$method () { $body ;}"
+#   tmp=( ${__methodh[$__class]} "$method" )
+#   __methodh[$__class]=${tmp[*]}
+#   __bodyh[$__class.$method]=$body
+# }
+#
+# class Class; {
+#   def new <<'  end'
+#     local class=$1; shift
+#     local self
+#
+#     for self in "$@"; do
+#       [[ -n ${__parenth[$class]} ]] && eval "${__parenth[$class]}".new "$@"
+#       __inherit "$class" "$self"
+#     done
+#   end
+# }
+#
+# class Object , Class; {
+#   def set <<'  end'
+#     local -n __self=$1; shift
+#
+#     __=''
+#     "$@"
+#     eval __self="$__"
+#   end
+#
+#   def to_s <<'  end'
+#     local self=$1
+#
+#     __=$(declare -p "$self" 2>/dev/null) || return
+#     __=${__#declare -* $self=}
+#   end
+# }
+#
+# class Array; {
+#   def append <<'  end'
+#     local -n __vals=$1; shift
+#     local __statement
+#
+#     case $# in
+#       '0' ) return;;
+#       '1' )
+#         printf -v __statement '__vals+=( "${%s[@]}" )' "$1"
+#         eval "$__statement"
+#         ;;
+#       * ) "$@"; __vals+=( "${__[@]}" );;
+#     esac
+#   end
+#
+#   def join <<'  end'
+#     local -n __vals=$1; shift
+#     local IFS=$1
+#
+#     __=${__vals[*]}
+#   end
+# }
+#
+# class Hash; {
+#   def map <<'  end'
+#     local -n __valh=$1
+#     local __keyparm=$3
+#     local __valparm=$4
+#     local __lambda=$5
+#     local "$__keyparm"
+#     local "$__valparm"
+#     local __key
+#     local __retvals=()
+#     local __statement
+#
+#     printf -v __statement '__retvals+=( "$(puts "%s")" )' "$__lambda"
+#
+#     for __key in "${!__valh[@]}"; do
+#       printf -v "$__keyparm" '%s' "$__key"
+#       printf -v "$__valparm" '%s' "${__valh[$__key]}"
+#       eval "$__statement"
+#     done
+#
+#     __=( "${__retvals[@]}" )
+#   end
+# }
+#
+# class String
+#
+# defs () { IFS=$'\n' read -rd '' "$1" ||: ;}
+#
+# puts () { printf '%s\n' "$1" ;}
+#
+# class File , Path; {
+#   def each <<'  end'
+#     local __filename=${!1}
+#     local __lineparm=$3
+#     local __lambda=${4:-$(</dev/stdin)}
+#     local "$__lineparm"
+#
+#     while read -r "$__lineparm"; do
+#       eval "$__lambda"
+#     done <"$__filename" ||:
+#   end
+#
+#   def write <<'  end'
+#     local __filename=${!1}; shift
+#     local __string
+#
+#     case $# in
+#       '0' ) return              ;;
+#       '1' ) __string=${!1}      ;;
+#       *   ) "$@"; __string=$__  ;;
+#     esac
+#     puts "$__string" >"$__filename"
+#   end
+# }
+#
+# class Path; {
+#   def expand_path <<'  end'
+#     local __pathname=${!1}
+#     local __filename
+#
+#     unset -v CDPATH
+#     [[ -e $__pathname ]] && {
+#       __filename=$(basename "$__pathname")
+#       __pathname=$(dirname "$__pathname")
+#     }
+#     [[ -d $__pathname ]] || return
+#     __pathname=$(cd "$__pathname"; pwd)
+#     __=$__pathname${__filename:+/}${__filename:-}
+#   end
+# }
 
 [[ -n $shpec_test ]] && return
 unset -v __class
--- shpec/rubsh_shpec.bash
+++ shpec/rubsh_shpec.bash
@@ -13,117 +13,95 @@ stop_on_error
 shpec_test=true
 shpec_source lib/rubsh.bash
 
-describe __
-  it "is blank"
-    assert equal 'declare -- __=""' "$(declare -p __)"
-  end
-end
+# describe class
+#   it "sets __class globally"; (
+#     class Sample
+#     assert equal Sample "$__class"
+#     return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
+#   end
+#
+#   it "calls Class.new"; (
+#     stub_command Class.new 'echo "$@"'
+#
+#     assert equal Sample "$(Class.new Sample)"
+#     return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
+#   end
+#
+#   it "assigns Object as the default parent"; (
+#     stub_command Class.new
+#
+#     class Sample
+#     assert equal Object "${__parenth[Sample]}"
+#     return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
+#   end
+#
+#   it "assigns the given parent"; (
+#     stub_command Class.new
+#
+#     class Sample , Other
+#     assert equal Other "${__parenth[Sample]}"
+#     return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
+#   end
+# end
+#
+# describe def
+#   it "creates a method"; (
+#     __class=Sample
+#     read -rd '' expected <<'    EOS' ||:
+# Sample.example () 
+# { 
+#     :
+# }
+#     EOS
+#     def example <<<:
+#     assert equal "$expected" "$(declare -f Sample.example)"
+#     return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
+#   end
+# end
 
-describe ___class
-  it "is not set"
-    stop_on_error off
-    declare -p ___class >/dev/null 2>&1
-    assert unequal 0 $?
-    stop_on_error
-  end
-end
-
-describe ___methodh
-  it "catalogues the methods of classes"
-    read -r expected <<'    EOS' ||:
-      declare -A __methodh='([Object]="set to_s" [Array]="append join" [Class]="new" [Hash]="map" [File]="each write" [Path]="expand_path" )'
-    EOS
-    assert equal "$expected" "$(declare -p __methodh)"
-  end
-end
-
-describe class
-  it "sets __class globally"; (
-    class Sample
-    assert equal Sample "$__class"
-    return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
-  end
-
-  it "calls Class.new"; (
-    stub_command Class.new 'echo "$@"'
-
-    assert equal Sample "$(Class.new Sample)"
-    return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
-  end
-
-  it "assigns Object as the default parent"; (
-    stub_command Class.new
-
-    class Sample
-    assert equal Object "${__parenth[Sample]}"
-    return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
-  end
-
-  it "assigns the given parent"; (
-    stub_command Class.new
-
-    class Sample , Other
-    assert equal Other "${__parenth[Sample]}"
-    return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
-  end
-end
-
-describe def
-  it "creates a method"; (
-    __class=Sample
-    read -rd '' expected <<'    EOS' ||:
-Sample.example () 
-{ 
-    :
-}
-    EOS
-    def example <<<:
-    assert equal "$expected" "$(declare -f Sample.example)"
-    return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
-  end
-end
-
-describe inherit
-  it "creates instance methods from a class"; (
-    inherit Class sample
-    assert equal $'declare -f sample.inherit\ndeclare -f sample.new' "$(declare -F | grep sample\.)"
-    return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
-  end
-end
-
-describe Class.new
-  it "creates Object methods"; (
-    Class.new Sample example
-    IFS=$'\n' read -rd '' -a results <<<"$(declare -F | grep example\.)" ||:
-    results=( "${results[@]#declare -f example.}" )
-    assert equal "${__methodh[Object]}" "${results[*]}"
-    return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
-  end
-
-  it "creates methods of the specified class in addition"; (
-    Class.new Class example
-    IFS=$'\n' read -rd '' -a results <<<"$(declare -F | grep example\.)" ||:
-    results=( "${results[@]#declare -f example.}" )
-    assert equal "inherit new set to_s" "${results[*]}"
-    return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
-  end
-
-  it "creates an instance-named function"; (
-    Class.new Sample example
-    declare -f example >/dev/null
+describe Class new
+  it "creates a new object"; (
+    Class new Sample
+    declare -f Sample >/dev/null
     assert equal 0 $?
     return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
   end
 
-  it "does so for multiple instances"; (
-    Class.new Sample example1 example2
-    declare -f example1 >/dev/null
-    results=( $? )
-    declare -f example2 >/dev/null
-    results+=( $? )
-    assert equal '0 0' "${results[*]}"
-    return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
-  end
+  # it "assumes new for an unspecified method"
+  #   Class Sample
+  #   declare -f Sample
+#   it "creates Object methods"; (
+#     Class.new Sample example
+#     IFS=$'\n' read -rd '' -a results <<<"$(declare -F | grep example\.)" ||:
+#     results=( "${results[@]#declare -f example.}" )
+#     assert equal "${__methodh[Object]}" "${results[*]}"
+#     return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
+#   end
+#
+#   it "creates methods of the specified class in addition"; (
+#     Class.new Class example
+#     IFS=$'\n' read -rd '' -a results <<<"$(declare -F | grep example\.)" ||:
+#     results=( "${results[@]#declare -f example.}" )
+#     assert equal "inherit new set to_s" "${results[*]}"
+#     return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
+#   end
+#
+#   it "creates an instance-named function"; (
+#     Class.new Sample example
+#     declare -f example >/dev/null
+#     assert equal 0 $?
+#     return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
+#   end
+#
+#   it "does so for multiple instances"; (
+#     Class.new Sample example1 example2
+#     declare -f example1 >/dev/null
+#     results=( $? )
+#     declare -f example2 >/dev/null
+#     results+=( $? )
+#     assert equal '0 0' "${results[*]}"
+#     return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
+#   end
 end
 
 # describe Object.new
