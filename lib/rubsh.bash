4464ee692cea78ebac6275f718a0b12b42ec26af
Ted Lilley <ted.lilley@accelerated.com>
2017-03-27 14:23:08 -0400

    feat: add Class and Object functions, make Class answer Object

--- lib/rubsh.bash
+++ lib/rubsh.bash
@@ -2,3 +2,6 @@
 [[ -n ${reload:-}                   ]] && { unset -v reload && echo reloaded || return ;}
 [[ -z ${_rubsh:-}                   ]] && readonly _rubsh=loaded
 
+Object () { :;}
+
+Class () { __='([0]="Object")' ;}
--- shpec/rubsh_shpec.bash
+++ shpec/rubsh_shpec.bash
@@ -12,3 +12,32 @@ stop_on_error
 shpec_test=true
 eval "source $shpec_cwd/../lib/rubsh.bash"
 
+is_function () { declare -f "$1" >/dev/null ;}
+
+describe Object
+  it "is a function"
+    is_function Object
+    assert equal 0 $?
+  end
+
+  describe ancestors
+    it "lists an array string of ancestor classes"
+      Object ancestors
+      assert equal '' "$__"
+    end
+  end
+end
+
+describe Class
+  it "is a function"
+    is_function Class
+    assert equal 0 $?
+  end
+
+  describe ancestors
+    it "lists an array string of ancestor classes"
+      Class ancestors
+      assert equal '([0]="Object")' "$__"
+    end
+  end
+end
