2fed29359bc5ad52c4fcab046e154378c4d60ea3
Ted Lilley <ted.lilley@accelerated.com>
2017-04-15 01:02:30 -0400

    feat: self

---
+++ shpec/predefined_shpec.bash
@@ -0,0 +1,22 @@
+source import.bash
+
+shpec_helper_imports=(
+  initialize_shpec_helper
+  shpec_cwd
+  stop_on_error
+)
+eval "$(importa shpec-helper shpec_helper_imports)"
+initialize_shpec_helper
+stop_on_error=true
+stop_on_error
+
+source "$(shpec_cwd)"/../lib/rubsh
+
+is_function () { declare -f "$1" >/dev/null 2>&1 ;}
+
+describe self
+  it "returns 'main' in the main context"
+    self
+    assert equal '"main"' "$__"
+  end
+end
